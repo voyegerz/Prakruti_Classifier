a857b4b0002ad8d49d43c15c8c6e5f18036d31b6
voyegerz <nirajkumaruiflow@gmail.com>
2023-09-17 18:40:41 +0530

    organised and fixed dataset

--- training.py
+++ training.py
@@ -1,67 +1,72 @@
 import pandas as pd
-import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Embedding, LSTM, Dense
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
+import random
 
-# step 2
 # Load the dataset
-df = pd.read_csv('new.csv')
-
-# Preprocess text data
-df['question'] = df['question'].str.lower()
-df['option'] = df['option'].str.lower()
+df = pd.read_csv('phenotype.csv')
 
 # Encode the 'class' column
 label_encoder = LabelEncoder()
 df['class'] = label_encoder.fit_transform(df['class'])
 
 # Tokenize the 'question' and 'option' columns
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(df['question'] + df['option'])
-df['question_sequence'] = tokenizer.texts_to_sequences(df['question'])
-df['option_sequence'] = tokenizer.texts_to_sequences(df['option'])
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(df['question'] + " " + df['option'])
 
-# Pad sequences
-max_sequence_length = max(len(seq) for seq in df['question_sequence'])
-df['question_sequence'] = pad_sequences(df['question_sequence'], maxlen=max_sequence_length)
-df['option_sequence'] = pad_sequences(df['option_sequence'], maxlen=max_sequence_length)
+# Train a model (e.g., Logistic Regression)
+model = LogisticRegression()
+model.fit(X, df['class'])
 
-# step 3
-# Combine question and option sequences
-X = np.hstack((df['question_sequence'].values, df['option_sequence'].values))
+# Define a function to ask a random set of questions with options
+def ask_random_questions_with_options(num_questions=20):
+    random_questions = random.sample(df['question'].tolist(), num_questions)
+    user_responses = []
+    
+    for i, question_text in enumerate(random_questions, 1):
+        question_options = df[df['question'] == question_text]
+        
+        # Select the top three options based on their relevance to the class
+        question_options = question_options.nlargest(3, 'class')
+        
+        options = question_options['option'].tolist()
+        class_labels = question_options['class'].tolist()
+        
+        print(f"Question {i}: {question_text}")
+        for j, option in enumerate(options, 1):
+            print(f"{j}. {option}")
+        
+        user_input = input("Select an option (1, 2, or 3): ")
+        
+        try:
+            selected_option = options[int(user_input) - 1]
+            selected_class = class_labels[int(user_input) - 1]
+            user_responses.append((selected_option, selected_class))
+        except (ValueError, IndexError):
+            print("Invalid option. Please select 1, 2, or 3.")
+    
+    return user_responses
 
-# Reshape X to match the expected input shape
-X = X.reshape(X.shape[0], -1)
+# Ask random questions with options
+print("Please answer the following questions (select options 1, 2, or 3):")
+user_responses = ask_random_questions_with_options()
 
-# Encode the 'class' column
-label_encoder = LabelEncoder()
-df['class'] = label_encoder.fit_transform(df['class'])
-y = to_categorical(df['class'], num_classes=len(label_encoder.classes_))
+# Process user responses
+processed_user_responses = [response[0] for response in user_responses]
 
-# Split the data into training, validation, and test sets
-X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+# Classify user based on responses
+processed_user_responses = vectorizer.transform(processed_user_responses)
+predicted_class = model.predict(processed_user_responses)
 
+# Calculate class percentages
+class_counts = pd.Series(predicted_class).value_counts()
+total_responses = len(predicted_class)
+class_percentages = class_counts / total_responses * 100
 
-# step 4
-# Define the RNN model
-model = Sequential()
-model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=100, input_length=max_sequence_length*2))
-model.add(LSTM(128))
-model.add(Dense(len(label_encoder.classes_), activation='softmax'))
+# Map class labels back to their original labels
+class_labels = label_encoder.inverse_transform(class_counts.index)
 
-# Compile the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
-
-# step 5
-# Evaluate the model on the test set
-loss, accuracy = model.evaluate(X_test, y_test)
-print(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.4f}")
+# Display class percentages
+for label, percentage in zip(class_labels, class_percentages):
+    print(f"{label}: {percentage:.2f}%")
